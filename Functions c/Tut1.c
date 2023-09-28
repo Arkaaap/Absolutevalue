af06243511e5314e23c0dec8e88ad4e372552bb2
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2023-09-28 20:03:40 +0530

    functions

--- Functions c/Tut1.c
+++ Functions c/Tut1.c
@@ -6,9 +6,45 @@ int add(int x,int y){
     return (x+y);
 }
 int main (){
+   
     int x=10,y=20;
     int z;
     z=(x+y);
-    printf("The result is %d",z);
+    printf("The sum of two integers are %d",z);
+   
+    
     return 0;
 }
+
+
+
+
+
+// #include <stdio.h>
+
+// int add (int x,int y);
+
+// int add(int x,int y){
+//     return (x+y);
+// }
+// int main (){
+//     int i;
+//     int x=10,y=20;
+//     int z;
+//     z=(x*y);
+   
+//     for (i=1;i<=5;i++){
+//     printf("The result is %d\n",z);}
+
+//     // if (x%y==0){
+//     //     printf("The program is good")
+//     // }
+//     return 0;
+// }
+
+
+
+
+
+
+ 
---
+++ c/Assignment/Assignment6func.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int add (int x,int y);
+
+int add(int x,int y){
+    return (x+y);
+}
+int main (){
+   
+    int x=10,y=20;
+    int z;
+    z=(x+y);
+    printf("The sum of two integers are %d",z);
+   
+    
+    return 0;
+}

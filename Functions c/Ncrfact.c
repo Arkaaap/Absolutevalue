4140afd478a4b2f3f0fed94ce0ae3e03af387308
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2023-10-17 21:12:48 +0530

    Pascal Triangle

--- Functions c/Ncrfact.c
+++ Functions c/Ncrfact.c
@@ -11,14 +11,12 @@ int main (){
     int r;
     printf("Enter the value of n : ");
     scanf("%d",&n);
-
     printf("Enter the value of r : ");
     scanf("%d",&r);
-    
-    int nfact=ncr(n);
-    int rfact =ncr(r);
-    int nrfact =ncr(n-r);
-    int factorial=nfact/(rfact*nrfact);
+    int nfact=ncr(n);//n!
+    int rfact =ncr(r);//r!
+    int nrfact =ncr(n-r);//(n-r!)
+    int factorial=nfact/(rfact*nrfact);//n!/r!/(n-r!)
    
     printf("%d",factorial);
     return 0;

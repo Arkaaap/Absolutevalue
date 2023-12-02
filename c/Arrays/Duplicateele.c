cda6292d6ef5811a38a4e3e4dabeee21d0fc9514
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2023-12-02 21:00:11 +0530

    Upadted

---
+++ c/Arrays/Duplicateele.c
@@ -0,0 +1,21 @@
+//FIND THE DUPLICATE ELEMENTS IN AN ARRAY ;
+
+
+#include <stdio.h>
+int main (){
+  int a[5];
+  int count =0;
+  int i;
+  for (int i=0;i<5;i++){
+    scanf ("%d",&a[i]);
+  }
+  for (int i=0;i<5;i++){
+    for (int j=i+1;j<5;j++){
+        if (a[i]==a[j]){
+            count++;
+        }
+    }
+  }
+  printf ("The duplicate elements are %d  ",count );
+
+}

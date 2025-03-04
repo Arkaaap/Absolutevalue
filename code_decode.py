cd7fc283caddb46d8f20f6d612889669782f142b
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2025-03-04 22:24:11 +0530

    Update code_decode.py

--- code_decode.py
+++ code_decode.py
@@ -5,7 +5,7 @@ Final_String = s[l-1] + s[1:l-1] +s[0]
 print (f"Encrypted string {Final_String}")
 print (f"Decrypted string {s}")
 
-'''Suppose a string is given as 'MEAT' encode the string like 'TEAM' 0 th and (n-1)'th element will be swapped and the rest of them will be as it's '''
+'''.2.Suppose a string is given as 'MEAT' encode the string like 'TEAM' 0 th and (n-1)'th element will be swapped and the rest of them will be as it's '''
 
 
 

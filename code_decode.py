00a84fb275cf288599c740982cdaec37cf38484d
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2025-03-04 21:20:39 +0530

    Create code_decode.py

---
+++ code_decode.py
@@ -0,0 +1,12 @@
+s = str(input("Enter The string :"))
+l = len(s)
+
+Final_String = s[l-1] + s[1:l-1] +s[0]
+print (f"Encrypted string {Final_String}")
+print (f"Decrypted string {s}")
+
+'''Suppose a string is given as 'MEAT' encode the string like 'TEAM' 0 th and (n-1)'th element will be swapped and the rest of them will be as it's '''
+
+
+
+

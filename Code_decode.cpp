890b840955607bc5e3416e26bb404e4eb0959a31
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2025-03-04 21:11:02 +0530

    Create Code_decode.cpp

---
+++ Code_decode.cpp
@@ -0,0 +1,28 @@
+/*If ‘WATER’ is written as ‘YCVGT’, then what is written as ‘HKTG’*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void Fun (char *c)
+{
+    for (char i =0 ;c[i]!='\0';i = i+1)//Looping from 0 to null terminator 
+    {
+        if (c[i]>='A' && c[i]<='Z')//checking if the string's current character is greater than a and less than z which means it's a valid charcter then increments the charatcter with +2
+        {
+            c[i]  = c[i]+2;
+        }
+        if (c[i]>'Z')
+        {
+            c[i] = c[i]-26;//if the charcter is not in the list round up to the A character again 
+        }
+    }
+}
+
+int main ()
+{
+    char c []= "WATER";
+    Fun(c);
+    printf ("%s\t",c);
+    return (0);
+}

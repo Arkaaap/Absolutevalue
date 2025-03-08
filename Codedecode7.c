42bb88297de2b96f40299dcd4c0ca5184b3f6e5f
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2025-03-08 19:31:04 +0530

    Create Codedecode7.c
    DATE:8/3/2025

---
+++ Codedecode7.c
@@ -0,0 +1,34 @@
+/*SOURCE :hitbullseye.com Author : Arkaaap
+    In a certain code 'SEQUENCE' is coded as 'FDOFVRFT. How is 'CHILDREN' coded in that code?
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void encode (char *c)
+{
+	fprintf (stdout,"Encoded String :\n");
+	int l = strlen(c);
+	for (int i=l ; i>=0; i--)
+	{
+	    if (c[i]>='A' && c[i]<='Z')
+	    {
+		printf ("%c",c[i]+1);
+	}
+	}
+
+
+}
+
+
+
+
+int main (void)
+{
+	char c[100];
+	printf ("Enter The String :");
+	scanf ("%s",c);
+	encode(c);
+}

c8ed0e8705586da726f8a73d8f5d09f8501fab80
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2025-03-13 11:54:40 +0530

    Create codeDecode8.c date:3/13/25

---
+++ codeDecode8.c
@@ -0,0 +1,39 @@
+/*In a certain code language “CONSTRUCTION” is written as “EMPQVPWAVGQL”. What will be the code for “DESTRUCTION” in the same language(even indecies +2 and odd -2 )*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void encode (char *c)
+{
+    int l = strlen(c);
+    char *c1 = (char *)malloc(l+1);
+    for (int i=0;i<l;i++)
+    {
+        if (i%2 == 0)
+        {
+            c1[i] = c[i]+2;
+        }
+        else {
+            c1[i] = c[i]-2;
+        }
+        
+        
+    }
+    
+    printf ("%s",c1);
+    
+    
+    
+}
+
+
+
+int main (void){
+    char c[100];
+    printf ("Enter The String :");
+    scanf ("%s",c);
+    encode(c);
+    return (0x001-1);
+}
